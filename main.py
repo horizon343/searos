58bbffa33e9eda434f9b99c17a08188e46562107
horizon343 <sh.maksim343@gmail.com>
2024-12-02 17:37:21 +0500

    update task_sql

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from db.context import get_db, engine
 from db.models.base_model import Base, StatusEnum
 from db.models.task_api_model import TaskApiCreate, TaskApi, TaskApiUpdate
-from db.models.task_sql_model import TaskSqlCreate, TaskSql
+from db.models.task_sql_model import TaskSqlCreate, TaskSql, TaskSqlUpdate
 from celery_folder.celery_tasks import execute_task_api, celery_app, execute_task_sql
 
 Base.metadata.create_all(bind=engine)
@@ -73,6 +73,28 @@ def add_task_sql(data: TaskSqlCreate, db: Session = Depends(get_db)):
         start_task(new_task_sql, db, execute_task_sql)
 
     return {"message": "Task added successfully"}
+
+
+@app.patch("/update_task_sql/{task_sql_id}")
+def update_task_sql(task_sql_id: int, task_sql_data: TaskSqlUpdate, db: Session = Depends(get_db)):
+    task_sql = db.query(TaskSql).filter(TaskSql.id == task_sql_id).first()
+    if not task_sql:
+        return {"message": "Task not found"}
+
+    update_task_data = task_sql_data.dict(exclude_unset=True)
+    for key, value in update_task_data.items():
+        setattr(task_sql, key, value)
+
+    if task_sql.task_celery_id:
+        celery_app.control.revoke(task_sql.task_celery_id, terminate=True, signal='SIGKILL')
+        task_sql.task_celery_id = None
+
+    db.commit()
+
+    if task_sql.status == StatusEnum.IN_PROGRESS:
+        start_task(task_sql, db, execute_task_sql)
+
+    return {"message": "Task updated successfully"}
 
 
 def start_task(new_task, db, execute_task):
